cfc56df5f9acdda3bc85d8ed828cf388642244a2
cetobi <andre.marcdias@gmail.com>
2022-03-08 19:22:50 -0300

    Atualizando exercicios 8 e 9

--- exerciciosJS/8-funcaoPontos.js
+++ exerciciosJS/8-funcaoPontos.js
@@ -1,24 +1,31 @@
-
+/*
+Dada uma lista string = “pontuação1 pontuação2
+pontuação3 etc..”, escreva uma função que ao recebê-la irá comparar os valores um a um e irá retornar um
+vetor com o número de vezes que ele bateu seu recorde de maior número de pontos e quando fez seu pior
+jogo. (Número do pior jogo).
+*/
 
 function pontos(lista) {
     let pontos = lista.split(',')
     let maiorPontuacao = 0
-    let menorPontuacao = 0
+    let menorPontuacao = pontos[0]
+    let rodada = 1
     for(let i=0; i < pontos.length; i++){
-        for(let j in pontos){
-            if(pontos[i] > pontos[j]){
-                maiorPontuacao++
-            }
-            else if(pontos[i] < pontos[j]){
-                menorPontuacao++
-            }
+        if(pontos[i+1] > pontos[i]){
+            maiorPontuacao++
+        }
+        if(pontos[i] < menorPontuacao){
+            menorPontuacao = pontos[i]
+            rodada = i+1
         }
     }
-
-
-    return [maiorPontuacao, menorPontuacao]
+    return [maiorPontuacao, rodada]
 }
 
-let b = '1,2,3,4,5,6'
+let a = '1,2,3,4,5,6'
+let b = '2,3,7,5,1,9'
+let c = '4,3,1,7,9,2'
 
+console.log(pontos(a))
 console.log(pontos(b))
+console.log(pontos(c))
--- exerciciosJS/9-funcaoNotas.js
+++ exerciciosJS/9-funcaoNotas.js
@@ -1,13 +1,28 @@
+/*
+Construa uma função para um sistema de notas de uma instituição que possui a seguinte política de
+classificação: Todo aluno recebe uma nota de 0 a 100. Alunos com nota abaixo de 40 são reprovados. As notas
+possuem a seguinte regra de arredondamento: Se a diferença entre a nota e o próximo múltiplo de 5 for menor
+que 3, arredondar a nota para esse próximo múltiplo de 5. Se a nota for abaixo de 38, não é feito nenhum
+arredondamento pois esta nota resulta na reprovação do aluno.
+*/
 
-
-function Nota(num){
-
-    let resto = num%5
-    if(resto < 4){
-        
+function Nota(num) {
+    if(num < 38){
+        console.log(`Reprovado!!! Nota: ${num}`);
     }
-
-    /*if(n < 40){
-        console.log('Reprovado por Nota!!!');
-    }*/
+    else{
+       let resto = num%5
+       if(resto <= 3) {
+           while(num%5 !== 0){
+               num++
+           }
+       }
+       console.log(`Aprovado!!! Nota: ${num}`);
+    }
 }
+
+Nota(90)
+Nota(38)
+Nota(23)
+Nota(59)
+Nota(79)
